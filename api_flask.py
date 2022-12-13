b8a93efcc0cd04a7b0175020bb91b747b700711d
Victória Rocha Brito <victoria.rochabrito@gmail.com>
2022-12-13 10:40:20 -0300

    Projeto finalizado

--- api_flask.py
+++ api_flask.py
@@ -41,38 +41,6 @@ class Users(Resource):  # user endpoint
             data.to_csv('users.csv', index=False)  # save back to CSV
             return {'data': data.to_dict()}, 200  # return data with 200 OK
 
-    def put(self):
-        parser = reqparse.RequestParser()  # initialize
-        parser.add_argument('userId', required=True)  # add args
-        parser.add_argument('location', required=True)
-        args = parser.parse_args()  # parse arguments to dictionary
-
-        # read our CSV
-        data = pd.read_csv('users.csv')
-
-        if args['userId'] in list(data['userId']):
-            # evaluate strings of lists to lists !!! never put something like this in prod
-            data['locations'] = data['locations'].apply(
-                lambda x: ast.literal_eval(x)
-            )
-            # select our user
-            user_data = data[data['userId'] == args['userId']]
-
-            # update user's locations
-            user_data['locations'] = user_data['locations'].values[0] \
-                .append(args['location'])
-
-            # save back to CSV
-            data.to_csv('users.csv', index=False)
-            # return data and 200 OK
-            return {'data': data.to_dict()}, 200
-
-        else:
-            # otherwise the userId does not exist
-            return {
-                'message': f"'{args['userId']}' user not found."
-            }, 404
-
     def delete(self):
         parser = reqparse.RequestParser()  # initialize
         parser.add_argument('userId', required=True)  # add userId arg
